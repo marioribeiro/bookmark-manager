85a67c2e98e073f088c40185c8e44e06b992c41d
Mário Ribeiro <mail@marioribeiro.com>
2017-05-31 21:30:28 +0100

    Update config for Heroku

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,3 +1,3 @@
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
