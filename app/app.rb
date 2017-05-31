27979af966a0b71b6c0e89e4c754467e3f4aa811
Mário Ribeiro <mail@marioribeiro.com>
2017-05-31 21:38:09 +0100

    Add Tag model and update Link to create a many to many relationship
    - Update new link form to include a field for tags

--- app/app.rb
+++ app/app.rb
@@ -2,6 +2,7 @@ ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra'
 require_relative 'models/link'
+require_relative 'models/tag'
 require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
@@ -16,7 +17,10 @@ class BookmarkManager < Sinatra::Base
   end
   
   post '/links' do
-    Link.create(url: params[:url], title: params[:title])
+    link = Link.new(url: params[:url], title: params[:title])
+    tag  = Tag.first_or_create(name: params[:tags])
+    link.tags << tag
+    link.save
     redirect '/links'
   end
 
--- app/models/link.rb
+++ app/models/link.rb
@@ -5,6 +5,8 @@ class Link
 
   include DataMapper::Resource
 
+  has n, :tags, through: Resource
+
   property :id,    Serial
   property :title, String
   property :url,   String
