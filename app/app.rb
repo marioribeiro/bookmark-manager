65dba142668c172bb7d24de1830a99aeb86e49e6
Mário Ribeiro <mail@marioribeiro.com>
2017-06-01 22:52:35 +0100

    Sort links by name when filtered by tag

--- app/app.rb
+++ app/app.rb
@@ -21,7 +21,7 @@ class BookmarkManager < Sinatra::Base
 
   get '/tags/:name' do
     tag = Tag.first(name: params[:name])
-    @links = tag ? tag.links : []
+    @links = tag ? tag.links(:order => [:title.asc]) : []
     erb :'links/index'
   end
   
