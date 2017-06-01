faa0b36fef064d6a8ddf9cb8a4e9c1c42a91a016
Mário Ribeiro <mail@marioribeiro.com>
2017-06-01 21:18:07 +0100

    Tweak layout.erb
    - Add route for '/'

--- app/app.rb
+++ app/app.rb
@@ -5,6 +5,10 @@ require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+  
   get '/links' do
     @links = Link.all
     erb :'links/index'
