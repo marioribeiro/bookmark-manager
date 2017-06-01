9266c3a42e040c137ca9a45fded3f5cf6184644d
Mário Ribeiro <mail@marioribeiro.com>
2017-06-01 20:37:19 +0100

    Refactor data_mapper_setup

--- app/app.rb
+++ app/app.rb
@@ -1,10 +1,6 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra'
-require 'data_mapper'
-require 'dm-postgres-adapter'
-require_relative 'models/link'
-require_relative 'models/tag'
 require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,3 +1,8 @@
+require 'data_mapper'
+require 'dm-postgres-adapter'
+require_relative 'models/link'
+require_relative 'models/tag'
+
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
